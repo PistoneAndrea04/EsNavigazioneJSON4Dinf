fce2ad77eb8388960c2c0f9f1cd9322dfa75860a
PistoneAndrea04 <a.pistone.1779@vallauri.edu>
2022-01-18 09:22:10 +0100

    Primo commit in assoluto

---
+++ README.md
@@ -0,0 +1,3 @@
+#PRIMO PROGETTO SU GITHUB#
+
+Questo è il primo progetto che caricherò su github
---
+++ index.js
@@ -0,0 +1,84 @@
+"use strict"
+
+let jsonDoc;
+let pos = 0;
+
+window.onload = function (){
+    let json = localStorage.getItem("jsonBookstore");
+    if (json != null) {
+        jsonDoc = JSON.parse(json);
+        console.log(jsonDoc);
+        visualizza();
+    }
+}
+
+function visualizza(){
+    let tBody = document.getElementById("tabLibri");
+    tBody.innerHTML = "";
+    for (let i=0; i<jsonDoc.length; i++){
+        let libro = jsonDoc[i];
+        let tr = document.createElement("tr");
+        tBody.append(tr);
+        //Scooriamo le chiavi e otteniamo ogni singolo valore
+        for (let key in libro){ //mi prende tutte le chiavi e me le scorre finop all'ultima
+            let td = document.createElement("td");
+            td.innerText = libro[key];
+            tr.append(td);
+        }
+        let td = document.createElement("td");
+        let btn = document.createElement("input");
+        btn.type = "button";
+        btn.value = "Elimina";
+        btn.setAttribute("id", i);
+        btn.addEventListener("click", function(){elimina(this.getAttribute("id"))})
+        td.append(btn);
+        tr.append(td);
+    }
+}
+
+function elimina(id){
+    jsonDoc.splice(id,1);
+    let jsonS = JSON.stringify(jsonDoc);
+    localStorage.setItem("jsonBookstore", jsonS);
+    visualizza();
+}
+
+function aggiungi(){
+    window.location.href = "aggiungi.html";
+}
+
+function leggiRecord(){
+    let div = document.getElementById("contenuto");
+    div.innerHTML = "";
+    for (let key in jsonDoc[pos]){
+        div.innerHTML += key + " : " + jsonDoc[pos][key] + "<br>";
+    }
+}
+
+function primo(){
+    pos = 0;
+    leggiRecord();
+}
+
+function ultimo(){
+    pos = jsonDoc.length-1;
+    leggiRecord();
+}
+
+function avanti(){
+    if (pos < jsonDoc.length-1) {
+        pos++;
+        leggiRecord();
+    }
+    else
+        alert("Sei già all'ultimo libro!");
+}
+
+function indietro(){
+    if (pos > 0) {
+        pos--;
+        leggiRecord();
+    }
+    else
+        alert("Sei già al primo libro!");
+}
